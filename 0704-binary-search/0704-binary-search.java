1659903235b803f6f5c24f4c15f99e964f877aef
domybest <114351464+bbororo5@users.noreply.github.com>
2025-03-06 17:46:04 +0900

    Time: 0 ms (100%), Space: 45.8 MB (45.08%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,10 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int search(int[] nums, int target) {
-        return IntStream.range(0, nums.length)
-        .filter(i -> nums[i] == target)
-        .findFirst()
-        .orElse(-1);
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
     }
 }
