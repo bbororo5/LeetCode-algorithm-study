68c201ac5991e251492cfd2c9948faa5b3dc073a
domybest <114351464+bbororo5@users.noreply.github.com>
2025-03-10 02:26:34 +0900

    Time: 0 ms (100%), Space: 40.5 MB (37.65%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+
+
+import com.sun.source.doctree.EscapeTree;
+
+class Solution {
+    public int climbStairs(int n) {
+        List<Integer> dp = new ArrayList<>();
+
+        dp.add(0, 1);
+        dp.add(1, 2);
+        dp.add(2, 3);
+        
+        if (n > 2) {
+            for (int i = 2; i < n; i++) {
+                dp.add(i, dp.get(i - 1) + dp.get(i - 2));
+            }
+        }
+        
+        return dp.get(n - 1);
+    }
+}
