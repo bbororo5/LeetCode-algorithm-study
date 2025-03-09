2c1a85aa442ac2e0cd161ca5b32c02ef05c2a0fc
domybest <114351464+bbororo5@users.noreply.github.com>
2025-03-10 02:27:55 +0900

    Time: 0 ms (100%), Space: 40.8 MB (17.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,9 +4,8 @@ class Solution {
 
         dp.add(0, 1);
         dp.add(1, 2);
-        dp.add(2, 3);
         
-        if (n > 2) {
+        if (n > 1) {
             for (int i = 2; i < n; i++) {
                 dp.add(i, dp.get(i - 1) + dp.get(i - 2));
             }
