f1c05839f497a8b97c9e3f25fdaa943cf42d87b7
domybest <114351464+bbororo5@users.noreply.github.com>
2025-05-03 03:20:22 +0900

    Time: 31 ms (13.81%), Space: 44.6 MB (72.5%) - LeetHub

---
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.java
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+};
+*/
+
+class Solution {
+    public Node lowestCommonAncestor(Node p, Node q) {
+        Deque<Node> pAncestors = findAncestors(p);
+        Deque<Node> qAncestors = findAncestors(q);
+
+        Node lca = null;
+
+        while (!pAncestors.isEmpty() && !qAncestors.isEmpty()) {
+            Node pAncestor = pAncestors.pollFirst();
+            Node qAncestor = qAncestors.pollFirst();
+
+            if (pAncestor == qAncestor) { 
+                lca = pAncestor; 
+            } else {
+                break;
+            }
+        }
+
+        return lca;
+    }
+
+    private Deque<Node> findAncestors(Node node) {
+        Deque<Node> ancestors = new ArrayDeque<>();
+        Node current = node;
+        while (current != null) {
+            ancestors.addFirst(current);
+            current = current.parent;
+        }
+
+        return ancestors;
+    }
+} 
