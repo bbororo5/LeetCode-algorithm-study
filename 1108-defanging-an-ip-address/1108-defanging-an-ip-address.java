e53c8a5349b7e2be3d96f87913f1e6cab82ad648
Jeon Seon Ung <114351464+bbororo5@users.noreply.github.com>
2025-12-26 01:49:26 +0900

    Time: 0 ms (100%), Space: 42.7 MB (69.01%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,5 +1,15 @@
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replace(".", "[.]");
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < address.length(); i++) {
+            char c = address.charAt(i);
+            if(c == '.') {
+                str.append("[.]");
+            } else {
+                str.append(c);
+            }
+        }
+
+        return str.toString();
     }
 }
