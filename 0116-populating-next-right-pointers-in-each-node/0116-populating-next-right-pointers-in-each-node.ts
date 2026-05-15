985d8b01f33c3fdcf0ae5816836db2170a1adb27
Jeon Seon Ung <114351464+bbororo5@users.noreply.github.com>
2026-05-15 16:23:38 +0900

    Time: 59 ms (63.81%), Space: 63.3 MB (19.05%) - LeetHub

---
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.ts
@@ -0,0 +1,38 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     left: _Node | null
+ *     right: _Node | null
+ *     next: _Node | null
+ *     constructor(val?: number, left?: _Node, right?: _Node, next?: _Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function connect(root: _Node | null): _Node | null {
+    if (!root) return null;
+
+    const queue: _Node[] = [root];
+
+    while (queue.length > 0) {
+        const levelSize = queue.length;
+
+        for (let i = 0; i < levelSize; i++) {
+            const node = queue.shift()!
+
+            if (i < levelSize - 1) {
+                node.next = queue[0];
+            }
+
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+    }
+    
+    return root; 
+};
