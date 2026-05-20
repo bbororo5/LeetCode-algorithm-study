ec2ae563a49cc8876877a4e01399cb9cce38af23
Jeon Seon Ung <114351464+bbororo5@users.noreply.github.com>
2026-05-20 17:36:03 +0900

    Time: 8 ms (10.61%), Space: 51.3 MB (92.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,41 @@
 class Solution {
-  void dfs(char[][] grid, int r, int c) {
-    int nr = grid.length;
-    int nc = grid[0].length;
+    public int numIslands(char[][] grid) {
+        int[] dr = {-1 , 1 , 0, 0};
+        int[] dc = {0, 0, -1, 1};
 
-    if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-      return;
-    }
+        int islandCount = 0;
 
-    grid[r][c] = '0';
-    dfs(grid, r - 1, c);
-    dfs(grid, r + 1, c);
-    dfs(grid, r, c - 1);
-    dfs(grid, r, c + 1);
-  }
+        int maxRow = grid.length;
+        int maxCol = grid[0].length;
 
-  public int numIslands(char[][] grid) {
-    if (grid == null || grid.length == 0) {
-      return 0;
-    }
+        Queue<int[]> queue = new LinkedList<>();
 
-    int nr = grid.length;
-    int nc = grid[0].length;
-    int num_islands = 0;
-    for (int r = 0; r < nr; ++r) {
-      for (int c = 0; c < nc; ++c) {
-        if (grid[r][c] == '1') {
-          num_islands++;
-          dfs(grid, r, c);
+        for (int r = 0; r < maxRow; r++) {
+            for (int c = 0; c < maxCol; c++) {
+                if(grid[r][c] == '1') {
+                    islandCount++;
+                    queue.add(new int[]{r, c});
+                    grid[r][c] = '0';
+
+                    while(!queue.isEmpty()) {
+                        int[] current = queue.poll();
+                        int curR = current[0];
+                        int curC = current[1];
+
+                        for (int i = 0; i < 4; i++) {
+                            int nextR = curR + dr[i];
+                            int nextC = curC + dc[i];
+                            
+                            if (nextR >= 0 && nextR < maxRow && nextC >= 0 && nextC < maxCol && grid[nextR][nextC] == '1') {
+                                queue.add(new int[]{nextR, nextC});
+                                grid[nextR][nextC] = '0';
+                            }
+                        }
+                    }
+                }
+            }
         }
-      }
-    }
 
-    return num_islands;
-  }
+        return islandCount;
+    }
 }
