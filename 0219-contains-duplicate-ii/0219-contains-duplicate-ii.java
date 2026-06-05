462c7eae944ca0dd5212aa21165d51b6578966d9
Jeon Seon Ung <114351464+bbororo5@users.noreply.github.com>
2026-06-05 15:36:59 +0900

    Time: 52 ms (8.98%), Space: 118.6 MB (5.16%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -15,7 +15,7 @@ class Solution {
         for(Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
             List<Integer> value = entry.getValue();
             for(int i = 1; i < value.size(); i++) {
-                if(Math.abs(value.get(i - 1) - value.get(i)) <= k) return true;
+                if(value.get(i) - value.get(i - 1) <= k) return true;
             }
         }
     
