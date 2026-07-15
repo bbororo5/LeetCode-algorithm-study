714ba0c1d680ba603b46fe6d31037835d742a053
Jeon Seon Ung <114351464+bbororo5@users.noreply.github.com>
2026-07-16 08:50:16 +0900

    Time: 3 ms (1.54%), Space: 49 MB (14.03%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,17 +1,18 @@
 class Solution {
     public int search(int[] nums, int target) {
+        Arrays.sort(nums);
         int left = 0;
         int right = nums.length - 1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
+        while(left <= right) {
+            int mid = (left + right) >>> 1;
+
+            if(nums[mid] > target) {
                 right = mid - 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            } else {
+                left = mid + 1;
             }
         }
 
