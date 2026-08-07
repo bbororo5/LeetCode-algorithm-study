988eacf59a547cb7e077a1c1305c02fbce76d32e
Jeon Seon Ung <114351464+bbororo5@users.noreply.github.com>
2026-08-07 14:10:28 +0900

    Time: 23 ms (0.92%), Space: 47.2 MB (11.25%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,36 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if (image[sr][sc] == color) {
+        int oldColor = image[sr][sc];
+
+        if(oldColor == color) {
             return image;
         }
-        
-        bfs(image, sr, sc, image[sr][sc], color);
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{sr, sc});
+        image[sr][sc] = color;
+
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int r = cur[0];
+            int c = cur[1];
+
+            System.out.printf(">> 현재 방문: (%d, %d)\n", r, c);
+            for(int i = 0; i < 4; i++) {
+                int nr = r + dx[i];
+                int nc = c + dy[i];
+
+                if(nr >= 0 && nr < image.length && nc >= 0 && nc < image[0].length && image[nr][nc] == oldColor) {
+                    queue.offer(new int[]{nr, nc});
+                    image[nr][nc] = color;
+                    System.out.printf("nr : %d, nc : %d, color: %d\n",nr, nc, image[nr][nc]);
+                }
+            }
+        }
 
         return image;
-    }
-
-    private void bfs(int[][] image, int sr, int sc, int oldColor, int newColor) {
-        int[] dr = new int[]{-1, 1, 0, 0};
-        int[] dc = new int[]{0, 0, -1, 1};
-        image[sr][sc] = newColor;
-
-        for(int i = 0; i < 4; i++) {
-            int nextR = sr + dr[i];
-            int nextC = sc + dc[i];
-
-            if(nextR >= 0 && nextR < image.length 
-                && nextC >= 0 && nextC < image[0].length
-                && image[nextR][nextC] == oldColor) {
-                    bfs(image, nextR, nextC, oldColor, newColor);
-                }
-        }
     }
 }
